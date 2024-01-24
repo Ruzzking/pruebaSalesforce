a4787e0da130408ae8f41680b7f8e1fb0e0bcab0
Ian juanchini <juanchiniezequiel@gmaill.com>
2024-01-24 11:39:07 -0300

    first

---
+++ force-app/main/default/lwc/lWCTest/lWCTest.js
@@ -0,0 +1,9 @@
+import { LightningElement } from 'lwc';
+
+export default class LWCTest extends LightningElement {
+    persona = {
+        firstName: "jose",
+        lastName: "perez",
+        age: 26
+    };
+}
